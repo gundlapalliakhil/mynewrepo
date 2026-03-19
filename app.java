0e85bd9ca657c0db7473c3e5087b0918ac006cd0
root <root@ip-172-31-24-103.ec2.internal>
2026-03-19 21:10:48 +0530

    java code added

--- app.java
+++ app.java
@@ -1,2 +1,8 @@
 This is advanced java file
 <h1> Java is long lengthy code</h1>
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
